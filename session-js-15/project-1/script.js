4e72c230c032bdf9d59fdde995f01fe6ffe99cca
rajesh <rajesh.t7982@gmail.com>
2024-06-23 09:37:42 +0530

    session-js-15

---
+++ session-js-15/project-1/script.js
@@ -0,0 +1,27 @@
+//variable declaration
+//dynamically typed language
+let a;
+
+console.log(a)
+console.log(typeof a)
+
+a=10;
+console.log(a)
+console.log(typeof a)
+
+a='hello'
+console.log(a)
+console.log(typeof a)
+
+a=true;
+console.log(a)
+console.log(typeof a)
+
+a=[1,2]
+console.log(typeof a)
+
+
+
+const b=123;
+
+b=200;
---
+++ session-js-15/project-2/script.js
@@ -0,0 +1,31 @@
+//arithmetic + - * / %
+let a=10;
+let b=3;
+console.log('sum is',a+b)
+console.log('quotient is ',a/b)
+console.log('remainder is ',a%b)
+
+
+//assignment operator
+//=
+
+//equal to operator
+//== ===
+console.log(a==b)
+
+let x=12;
+let y='12';
+console.log(typeof x)//number
+console.log(typeof y)//string
+
+console.log(x==y)
+console.log(x===y)
+
+//comparision
+//> >= < <= != !==
+
+//logical 
+// && ||  !
+
+console.log(a>5 && b<4)
+console.log(a>5 || b>4)
