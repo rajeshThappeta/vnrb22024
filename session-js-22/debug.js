ea09def9f6bfaad4edccc681654c74c0ad270108
rajesh <rajesh.t7982@gmail.com>
2024-08-22 20:21:57 +0530

    session-js-22

---
+++ session-js-22/debug.js
@@ -0,0 +1,41 @@
+//debug JavaScript app
+function compare(a,b){
+    if(a===b){
+        return "both equal"
+    }else if(a>b){
+        return `${a} is big` 
+    }else{
+        return `${b} is big` 
+    }
+}
+
+
+let result=compare(10,20);
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//template literal
+// let a=10;
+// let b=20;
+// let c=30;
+// console.log(" a is ",a,"  b is ",b," and c is ",c)
+// console.log(`a is ${a}  b is ${b} and c is ${c}`) //back tick 
+
+
---
+++ session-js-22/sync-async.js
@@ -0,0 +1,35 @@
+//sync version
+console.log("Three customers went restaurant");
+console.log("cust-1 ordered biryani");
+for (let index = 0; index < 4000000000; index++) {}
+
+console.log("cust-1 received biryani");
+
+console.log("cust-2 ordered soup");
+for (let index = 0; index < 20000000000; index++) {}
+
+console.log("cust-2 received soup");
+
+console.log("cust-3 ordered water bottle");
+for (let index = 0; index < 100000; index++) {}
+
+console.log("cust-3 received water bottle");
+
+//async version 20 22 29  36  39  31  24
+// console.log("Three customers went restaurant");
+
+// console.log("cust-1 ordered biryani");
+// setTimeout(() => {
+//   console.log("cust-1 received biryani");
+// }, 5000);
+
+// console.log("cust-2 ordered soup");
+// setTimeout(() => {
+//   console.log("cust-2 received soup");
+// }, 3000);
+
+// console.log("cust-3 ordered water bottle");
+
+// setTimeout(() => {
+//   console.log("cust-3 received water bottle");
+// }, 1000);
