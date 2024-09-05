a10400491eafe6f5ded5b607ea47a3b9557aa272
rajesh <rajesh.t7982@gmail.com>
2024-09-05 20:26:42 +0530

    react-session-1

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  //business logic
+  //return react element
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+      <p>This is a para</p>
+    </div>
+  );
+}
+
+export default App;
